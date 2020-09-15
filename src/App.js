6bbe6c131eea7d09751852fa133ad1d76369f5db
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-15 19:16:43 +0100

    api call complete, gets data correctly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import SongsContainer from './containers/SongsContainer';
 
 function App() {
   return (
     <div className="App">
       <>
         <h1 id="site-title">Top 20 Chart</h1>
-        
+        <SongsContainer />
       </>
     </div>
   );
--- src/containers/SongsContainer.jsx
+++ src/containers/SongsContainer.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import SongDetails from '../components/SongDetails';
+import SongsList from '../components/SongsList';
 
 class SongsContainer extends Component {
-  state = {  }
+
+  state = { 
+    songs: [],
+    selectedSongId: null
+  }
+
+  componentDidMount() {
+    fetch("https://itunes.apple.com/gb/rss/topsongs/limit=20/json")
+      .then(res => res.json())
+      .then(data => this.setState({ songs: data.feed.entry }))
+  }  
+
   render() { 
     return (
       <>
+        <SongsList />
+        <SongDetails />
       </>
     );
   }
+  
 }
  
 export default SongsContainer;
